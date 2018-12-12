34a96f48ec03ed6c67c1c52a1a5edd4ada58a647
Richard Leung <liang_yuxuan@163.com>
2018-12-12 23:13:01 +0800

    整理过。

--- src/gettoken.php
+++ src/gettoken.php
@@ -1,5 +1,4 @@
 <?php
-// require "fun_curl.php";
 header("content-type:text/html;charset=utf-8");
 
 $key = $_POST['key'];
@@ -10,12 +9,11 @@ $data = array(
     "secret" => $screct,
     "expiresIn" => 3600
 );
+
 $data = json_encode($data);
 $headerArray = array("Content-type:application/json;charset='utf-8'", "Accept:application/json");
 $curl = curl_init();
-//   $url='https://api.bi.tuputech.com/v1/auth/token/tgr3cu3q1r63ib85k7lbrcchp1vu1elolbpbqrgl';
 $url = 'https://api.bi.tuputech.com/v1/auth/token/' . $key;
-// echo $url;
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
