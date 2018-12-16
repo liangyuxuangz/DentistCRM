6820091e1d84279d3d8b849828885aad9137b671
Richard Leung <liang_yuxuan@163.com>
2018-12-16 23:13:52 +0800

    增加对数据库的“新记录插入”功能

--- src/fun.php
+++ src/fun.php
@@ -24,6 +24,36 @@ function http_build_query2($data){
     return http_build_query($obj);
 }
 
+function newsql($servername, $username, $password, $dbname, $sqlstr)
+{
+    // 创建连接
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // 检测连接
+    if ($conn->connect_error) {
+        die("连接失败: " . $conn->connect_error);
+    }
 
+    if ($conn->query($sqlstr) === true) {
+        echo "新记录插入成功";
+    } else {
+        echo "Error: " . $sqlstr . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+
+function create_sqlstr($formatstr, $replacedict)
+{
+    $tmp=$formatstr;
+    foreach ($replacedict as $x=>$x_value){
+        //echo $x;
+        //echo '<br/>';
+        //echo $x_value;
+        //echo '<br/>';
+        $tmp=str_replace($x, $x_value, $tmp);
+    }
+    return $tmp;
+    //echo $tmp;
+}
 
 ?>
