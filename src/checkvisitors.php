597c1c4f8de172a641140a213baa44c7279a5d5e
Richard Leung <liang_yuxuan@163.com>
2018-12-26 08:12:06 +0800

    修改了误笔的bug

---
+++ src/checkvisitors.php
@@ -0,0 +1,14 @@
+<?php
+require 'fun.php';
+
+header("content-type:text/html;charset=utf-8");
+
+$tag = $_POST['tag'];
+$name = $_POST['name'];
+$sex = $_POST['sex'];
+$age = $_POST['age'];
+
+echo $tag;
+
+
+?>
