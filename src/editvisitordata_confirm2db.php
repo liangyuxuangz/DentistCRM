abced710d0f4803892864ad98a80329743693e87
Richard Leung <liang_yuxuan@163.com>
2019-01-06 19:09:05 +0800

    增加了数据更新功能

---
+++ src/editvisitordata_confirm2db.php
@@ -0,0 +1,32 @@
+<?php
+
+require 'fun.php';
+
+header("content-type:text/html;charset=utf-8");
+
+$personId=$_POST['personId'];
+$usercode=$_POST['usercode'];
+$name=$_POST['name'];
+$type=$_POST['type'];
+$sex=$_POST['sex'];
+$age=$_POST['age'];
+
+$servername = "www.dcrm.com";
+$username = "root";
+$password = "root";
+$dbname = "dcrm";
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+} 
+$formatstr = "UPDATE `visitors` SET `usercode`='key0',`name`='key1',`type`='key2',`sex`='key3',`age`='key4' WHERE `personId`='key5'";
+$valuedict=array("key0"=>$usercode, "key1"=>$name, "key2"=>$type, "key3"=>$sex, "key4"=>$age, "key5"=>$personId);
+$sql=create_sqlstr($formatstr, $valuedict);
+echo $sql;
+
+$result=$conn->query($sql);
+
+$conn->close();
+
+?>
