aa21406876a521eaf1bea9c9c23faba20eb267c9
Richard Leung <liang_yuxuan@163.com>
2019-01-09 21:55:32 +0800

    关闭了echo

--- src/editvisitordata_confirm2db.php
+++ src/editvisitordata_confirm2db.php
@@ -23,7 +23,7 @@ if ($conn->connect_error) {
 $formatstr = "UPDATE `visitors` SET `usercode`='key0',`name`='key1',`type`='key2',`sex`='key3',`age`='key4' WHERE `personId`='key5'";
 $valuedict=array("key0"=>$usercode, "key1"=>$name, "key2"=>$type, "key3"=>$sex, "key4"=>$age, "key5"=>$personId);
 $sql=create_sqlstr($formatstr, $valuedict);
-echo $sql;
+//echo $sql;
 
 $result=$conn->query($sql);
 
