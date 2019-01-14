412149d93538e25d353c2df0c572b071fc7ee0f4
Richard Leung <liang_yuxuan@163.com>
2019-01-14 21:34:32 +0800

    改变量值

--- src/gettoken.php
+++ src/gettoken.php
@@ -10,7 +10,7 @@ $data = array(
     "expiresIn" => 3600
 );
 
-$data = json_encode($data);
+$jsondata = json_encode($data);
 $headerArray = array("Content-type:application/json;charset='utf-8'", "Accept:application/json");
 $curl = curl_init();
 $url = 'https://api.bi.tuputech.com/v1/auth/token/' . $key;
@@ -18,7 +18,7 @@ curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $jsondata);
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headerArray);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 $output = curl_exec($curl);
