d17690f21bd0cb4c119065778dbad944c5f2ba38
Richard Leung <liang_yuxuan@163.com>
2019-01-16 22:52:55 +0800

    增加了通过人员ID或姓名去统计。

--- src/getvisitcnt.php
+++ src/getvisitcnt.php
@@ -9,16 +9,52 @@
     $UID=$_POST['UID'];
     $SID=$_POST['SID'];
 
-    //echo $token;
+    if ($personId<>""){
+        //建立数据数组对象（字典）
+        $query = array('UID' => $UID, 'SID' => $SID);
+        //数据对象json化
+        $querystr = http_build_query2($query);
+        //建立头部信息数组对象
+        $headerArray = array("Content-type:application/json", "Accept:application/json", 'token:' . $token);
+        //发送的目标地址
+        $url = 'https://api.bi.tuputech.com/v2/flow/visitor/' . $personId . '?' . $querystr;
+    }
 
-    //建立数据数组对象（字典）
-    $query = array('UID' => $UID, 'SID' => $SID);
-    //数据对象json化
-    $querystr = http_build_query2($query);
-    //建立头部信息数组对象
-    $headerArray = array("Content-type:application/json", "Accept:application/json", 'token:' . $token);
-    //发送的目标地址
-    $url = 'https://api.bi.tuputech.com/v2/flow/visitor/' . $personId . '?' . $querystr;
+    if ($name<>""){
+        $servername = "www.dcrm.com";
+        $username = "root";
+        $password = "root";
+        $dbname = "dcrm";
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("连接失败: " . $conn->connect_error);
+        } 
+        $formatstr = "SELECT `personId` FROM `visitors` WHERE `name`='key0'";
+        $valuedict=array("key0"=>$name);
+        $sql=create_sqlstr($formatstr, $valuedict);
+
+        $result=$conn->query($sql);
+        //$personId=$result;
+        $row = $result->fetch_assoc();
+
+        $personId=$row['personId'];
+        //echo $personId;
+
+        $conn->close();
+
+        //建立数据数组对象（字典）
+        $query = array('UID' => $UID, 'SID' => $SID);
+        //数据对象json化
+        $querystr = http_build_query2($query);
+        //建立头部信息数组对象
+        $headerArray = array("Content-type:application/json", "Accept:application/json", 'token:' . $token);
+        //发送的目标地址
+        $url = 'https://api.bi.tuputech.com/v2/flow/visitor/' . $personId . '?' . $querystr;
+
+    }
+
+
     //curl初始化
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -28,8 +64,11 @@
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headerArray);
     //执行GET动作，并返回结构
     $output = curl_exec($ch);
-    echo $output;
-    //echo count($output.visits);
+    //echo $output;
+    $obj=json_decode($output);
+    var_dump($obj->visits[0]->start);
+    // echo $obj.visits;
+    //echo $obj['visits'][0];
     //do something
     //do something
 
